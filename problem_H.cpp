d315cda339f854334734ed4c8245c8c0141453b3
KseniyaShestakova <91065721+KseniyaShestakova@users.noreply.github.com>
2021-12-17 16:02:20 +0300

    Update problem_H.cpp

--- problem_H.cpp
+++ problem_H.cpp
@@ -1,22 +1,17 @@
-/*Шестакова Ксения, Б05-126
-https://codeforces.com/group/PVbQ8eK2T4/contest/350943/submission/134496810
-
-
-Дано число N и последовательность из N целых чисел. Найти вторую порядковую статистику на заданных диапазонах.
-
-Для решения задачи используйте структуру данных Sparse Table. Требуемое время обработки каждого диапазона O(1). Время подготовки структуры данных O(nlogn).
-
-Входные данные
-В первой строке заданы 2 числа: размер последовательности N и количество диапазонов M.
-
-Следующие N целых чисел задают последовательность. Далее вводятся M пар чисел - границ диапазонов.
-
-Выходные данные
-Для каждого из M диапазонов напечатать элемент последовательности - 2ю порядковую статистику. По одному числу в строке.
-*/
-
 #include <iostream>
-int getSecond( int l, int r, int** first, int** second, int* deg,int* arr){
+
+class SparseTable{
+    int number_of_elements;
+    int** first;
+    int** second;
+    int* deg;
+    int* arr;
+public:
+    SparseTable(int* arr, int n);
+    int getSecondMin(int l, int r);
+    ~SparseTable();
+};
+int SparseTable::getSecondMin( int l, int r){
     if ( r-l+1==2){
         if (std::min( arr[l], arr[r])==arr[l]) return arr[r];
         else return arr[l];
@@ -37,10 +32,107 @@ int getSecond( int l, int r, int** first, int** second, int* deg,int* arr){
         first_stat=first[pow][r+1-(1<<pow)];//случай, когда максимум слева не максимум всего отрезка
         return std::min( arr[first[pow][l]], arr[second[pow][r+1-(1<<pow)]]);
     }
-
-
 }
 
+int log_2(int i){
+    if (i==0) return 0;
+    int answer= log_2(i-1);
+    if ( !((i+1)&i)){
+        answer++;
+    }
+    return answer;
+}
+
+SparseTable::SparseTable(int *arr, int n) {
+    number_of_elements=n;
+    deg=new int[number_of_elements+1];
+
+    for (int i=0; i<number_of_elements; i++){
+        deg[i]= log_2(i);
+    }
+    /*
+    Исходная версия подсчета массива логарифмов, чуть более быстрая, т.к подсчитывает очередное число за O(1)
+    deg[0]=0;
+    for (int i=1; i<number_of_elements+1; i++){
+        deg[i]=deg[i-1];
+        if ( !((i+1)&i)){
+            deg[i]++;
+        }
+    }*/
+    int k=deg[number_of_elements-1];
+    first=new int*[k+1];
+    second=new int*[k+1];
+    for (int i=0; i<=k; i++){
+        first[i]=new int[number_of_elements];
+        second[i]=new int[number_of_elements];
+    }
+    for (int i=0; i<number_of_elements; i++){
+        first[0][i]=i;
+        second[0][i]=i;
+    }
+    for (int i=0; i<number_of_elements-1; i++){
+        if (std::min(arr[i], arr[i+1])==arr[i]){
+            first[1][i]=i;
+            second[1][i]=i+1;
+        }else{
+            first[1][i]=i+1;
+            second[1][i]=i;
+        }
+    }
+    first[1][number_of_elements-1]=number_of_elements-1;
+    second[1][number_of_elements-1]=number_of_elements-1;
+    for ( int j=2; j<=k; j++){
+        for (int i=0; i<number_of_elements; i++){
+            if ( i+(1<<(j-1))>=number_of_elements){
+                first[j][i]=first[j-1][i];
+                second[j][i]=second[j-1][i];
+                continue;
+            }
+            if (i+(1<<(j-1))==number_of_elements-1){
+                if ( std::min( arr[first[j-1][i]], arr[first[j-1][number_of_elements-1]])== arr[first[j-1][i]]){
+                    first[j][i]=first[j-1][i];
+                    if ( std::min( arr[first[j-1][number_of_elements-1]], arr[second[j-1][i]])==arr[first[j-1][number_of_elements-1]]){
+                        second[j][i]=first[j-1][number_of_elements-1];
+                        continue;
+                    }
+                    second[j][i]=second[j-1][i];
+                    continue;
+                }
+                first[j][i]=first[j-1][number_of_elements-1];
+                second[j][i]=first[j-1][i];
+                continue;
+            }
+            if ( std::min( arr[first[j-1][i]], arr[first[j-1][i+(1<<(j-1))]] )==arr[first[j-1][i]]){
+                first[j][i]=first[j-1][i];
+                if ( std::min( arr[second[j-1][i]], arr[first[j-1][i+(1<<(j-1))]] )==arr[second[j-1][i]]){
+                    second[j][i]=second[j-1][i];
+                    continue;
+                }
+                second[j][i]=first[j-1][i+(1<<(j-1))];
+                continue;
+            }
+            int border=i+(1<<(j-1));
+            first[j][i]=first[j-1][border];
+            if ( std::min( arr[first[j-1][i]], arr[second[j-1][i+(1<<(j-1))]])==arr[second[j-1][i+(1<<(j-1))]]){
+                second[j][i]=second[j-1][i+(1<<(j-1))];
+                continue;
+            }
+            second[j][i]=first[j-1][i];
+        }
+    }
+}
+
+SparseTable::~SparseTable() {
+    int k= deg[number_of_elements-1];
+    for (int i=0; i<=k; i++){
+        delete[] first[i];
+        delete[] second[i];
+    }
+    delete[] first;
+    delete[] second;
+    delete[] deg;
+    delete[] arr;
+}
 int main() {
     int n;
     std::cin>> n;
@@ -50,92 +142,15 @@ int main() {
     for (int i=0; i<n; i++){
         std::cin>> arr[i];
     }
-    int* deg=new int[n+1];
-    deg[0]=0;
-    for (int i=1; i<n+1; i++){
-        deg[i]=deg[i-1];
-        if ( !((i+1)&i)){
-            deg[i]++;
-        }
-    }
-    int k=deg[n-1];
-    int** first=new int*[k+1];
-    int** second=new int*[k+1];
-    for (int i=0; i<=k; i++){
-        first[i]=new int[n];
-        second[i]=new int[n];
-    }
-    for (int i=0; i<n; i++){
-        first[0][i]=i;
-        second[0][i]=i;
-    }
-    for (int i=0; i<n-1; i++){
-        if (std::min(arr[i], arr[i+1])==arr[i]){
-            first[1][i]=i;
-            second[1][i]=i+1;
-        }else{
-            first[1][i]=i+1;
-            second[1][i]=i;
-        }
-    }
-    first[1][n-1]=n-1;
-    second[1][n-1]=n-1;
-    for ( int j=2; j<=k; j++){
-        for (int i=0; i<n; i++){
-            if ( i+(1<<(j-1))>=n){
-                first[j][i]=first[j-1][i];
-                second[j][i]=second[j-1][i];
-            }else{
-                if (i+(1<<(j-1))==n-1){
-                    if ( std::min( arr[first[j-1][i]], arr[first[j-1][n-1]])== arr[first[j-1][i]]){
-                        first[j][i]=first[j-1][i];
-                        if ( std::min( arr[first[j-1][n-1]], arr[second[j-1][i]])==arr[first[j-1][n-1]]){
-                            second[j][i]=first[j-1][n-1];
-                        }else{
-                            second[j][i]=second[j-1][i];
-                        }
-                    }else{
-                        first[j][i]=first[j-1][n-1];
-                        second[j][i]=first[j-1][i];
-                    }
-                }else{
-                    if ( std::min( arr[first[j-1][i]], arr[first[j-1][i+(1<<(j-1))]] )==arr[first[j-1][i]]){
-
-                        first[j][i]=first[j-1][i];
-                        if ( std::min( arr[second[j-1][i]], arr[first[j-1][i+(1<<(j-1))]] )==arr[second[j-1][i]]){
-                            second[j][i]=second[j-1][i];
-                        }else{
-                            second[j][i]=first[j-1][i+(1<<(j-1))];
-                        }
-                    }else{
-                        int border=i+(1<<(j-1));
-
-                        first[j][i]=first[j-1][border];
-                        if ( std::min( arr[first[j-1][i]], arr[second[j-1][i+(1<<(j-1))]])==arr[second[j-1][i+(1<<(j-1))]]){
-                            second[j][i]=second[j-1][i+(1<<(j-1))];
-                        }else{
-                            second[j][i]=first[j-1][i];
-                        }
-                    }
-                }
-                }
-        }
-    }
+    SparseTable MySparseTable(arr, n);
     int l;
     int r;
     for (int i=0; i<m; i++){
         std::cin>>l;
         std::cin>>r;
-        std::cout<< getSecond(l-1, r-1, first, second, deg, arr)<<'\n';
+        std::cout<< MySparseTable.getSecondMin(l-1, r-1)<<'\n';
     }
-    for (int i=0; i<=k; i++){
-        delete[] first[i];
-        delete[] second[i];
-    }
-    delete[] first;
-    delete[] second;
-    delete[] deg;
-    delete[] arr;
+    MySparseTable.~SparseTable();
 
     return 0;
 }
